0fdd7cc8bca6618d5097eaaa55083785040a0cc0
Sridharan15 <sridharan987@gmail.com>
2020-03-17 11:37:56 +0530

    modified

---
+++ Find_occurences_of_characters.swift
@@ -0,0 +1,10 @@
+let paragraph = """
+And since Swift strings have full support for Unicode, thi
+""" .lowercased()
+var dict = [String: Int]()
+for each in paragraph {
+    if each.isLetter { 
+        dict["\(each)", default: 0] += 1
+    }
+}
+print(dict)
