1cef404226cbec7d937808dc95620fc34717124c
Sridharan T <51164940+Sridharan15@users.noreply.github.com>
2020-04-07 18:44:00 +0530

    Zombie Task 2 - The Advanced Gun

--- zombieTaskWithAdvancedGun.swift
+++ zombieTaskWithAdvancedGun.swift
@@ -13,7 +13,7 @@ var each = 0
 var distanceAndPowerOfZombies: [[Int]] = [[20,1],[25,2],[30,3],[35,4],[40,5],[45,6],[50,7],[55,8],[60,9],[65,10],[70,40]].sorted {$0[0] < $1[0]}
 while(each >= 0) {
     if distanceAndPowerOfZombies[each][0] == 0 || bullets <= 0 {
-        print("Game Over!! Killed:",totalKills,"Remaining Bullets:",bullets,advancedGun)
+        print("Game Over!! Killed:",totalKills,"Remaining Bullets:",bullets,"Remaining Advanced Gun:",advancedGun)
         break
     }
     if distanceAndPowerOfZombies[each][0] < zombieKillDistance && distanceAndPowerOfZombies[each][1] < advancedGunUsagePower {
@@ -30,7 +30,7 @@ while(each >= 0) {
         }
         print(advancedGun)
         if (distanceAndPowerOfZombies.isEmpty) {
-            print("Game Over!! Killed:",totalKills,"Remaining Bullets:",bullets,advancedGun)
+            print("Game Over!! Killed:",totalKills,"Remaining Bullets:",bullets,"Remaining Advanced Gun:",advancedGun)
             break
         }
         else {
@@ -60,7 +60,7 @@ while(each >= 0) {
         }
         distanceAndPowerOfZombies.remove(at: each)
         if (distanceAndPowerOfZombies.isEmpty) {
-            print("Game Over!! Killed:",totalKills,"Remaining Bullets:",bullets,advancedGun)
+            print("Game Over!! Killed:",totalKills,"Remaining Bullets:",bullets,"Remaining Advanced Gun:",advancedGun)
             break
         }
         else {
