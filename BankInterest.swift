1f2181ec1d31cd1436b7f3162f2c0a2d3d58b016
Sridharan T <51164940+Sridharan15@users.noreply.github.com>
2020-04-13 21:04:41 +0530

    bankInterestCalculator

--- BankInterest.swift
+++ BankInterest.swift
@@ -1,4 +1,14 @@
+/*Problem Statement: There’s bank in the name of Ivy Bank. Bank’s Balance 100,00,00,000.
+                     Person X has an account in Ivy Bank. His/her account number is XXXXXXXXXX.
+                     And Person X taken a loan amount of 25,00,000 for an interest rate of 14% and the loan tenure is 120 months. 
+                     Every month interest has to be recalculated from the principal amount. 
+                     At the end of 120 month, how much interest will be paid by the Person X. 
+                     Also at the end of 120 months what will be the Banks’s balance.
+                     If loan amount is higher than bBank balance Don't provide service.
+                     Calculate  using both simple and compound interest.*/
+
 import Foundation
+//to find interest compounded
 func compounds(compounded: String) -> (Double) {
     var n: Int = 0
     if compounded == "yearly" {
@@ -21,11 +31,13 @@ func compounds(compounded: String) -> (Double) {
     }
     return Double(n)
 }
+//to calculate compound Interest
 func compoundInterestCalculator(principle: Double,rate: Double,period: Double) -> Double {
     let n: Double = compounds(compounded: "monthly")
     let finalAmountToBePaid = principle * (pow((1 + (rate / (n * 100))),(n * period)))
     return finalAmountToBePaid
 }
+//to calculate simple Interest
 func simpleInterestCalculator(principle: Double,rate: Double,period: Double) -> Double {
     let interest: Double = (principle * rate * period) / 100
     return interest
