d7883f9a8d89e75d42a819f5bd03f60b78e87818
Sridharan T <51164940+Sridharan15@users.noreply.github.com>
2020-04-20 10:39:38 +0530

    electricityBillCalculator

--- electricityBillCalculator.swift
+++ electricityBillCalculator.swift
@@ -13,11 +13,17 @@ Fixed charges for two months Rs.290/service
 
 Get random numbers as daily consumable units. For domestic random number generation should be in between 1 - 10. For commercial 10 - 100*/
 
-let billType = "domestic"
-var amount : Float = 0
-var perDayUnitForTwoMonths: [Int] = []
-if billType == "domestic" {
+enum type: String {
+    case domestic,commercial
+}
+func domestic() {
+    var amount : Float = 0
+    var perDayUnitForTwoMonths: [Int] = []
     let serviceCharge: Float = 50
+    let rateOfFirstHundredUnits = 0
+    let rateOfNextHundredUnits: Float = 3.50
+    let rateOfNextThreeHundredUnits: Float = 4.60
+    let rateAboveFiveHundredUnits: Float = 6.60
     for each in 0..<60 {
         let perDayUnit = Int.random(in:1...10)
         perDayUnitForTwoMonths.append(perDayUnit)
@@ -25,22 +31,26 @@ if billType == "domestic" {
     let units = perDayUnitForTwoMonths.reduce(0,+)
     print("Units:",units)
     if units <= 100 {
-        amount = Float(units) * 0
+        amount = Float(units * rateOfFirstHundredUnits)
     }
     else if units >= 101 && units <= 200 {
-        amount = Float(100 * 0) + (Float(units - 100) * 3.50)
+        amount = Float(100 * rateOfFirstHundredUnits) + (Float(units - 100) * rateOfNextHundredUnits)
     }
     else if units >= 201 && units <= 500 {
-        amount = Float(100 * 0) + (Float(200 - 100) * 3.50) + (Float(units - 200) * 4.60)
+        amount = Float(100 *  rateOfFirstHundredUnits) + (Float(200 - 100) * rateOfNextHundredUnits) + (Float(units - 200) * rateOfNextThreeHundredUnits)
     }
     else {
-        amount = Float(100 * 0) + (Float(200 - 100) * 3.50) + (Float(500 - 200) * 4.60) + (Float(units - 500) * 6.60)
+        amount = Float(100 *  rateOfFirstHundredUnits) + (Float(200 - 100) * rateOfNextHundredUnits) + (Float(500 - 200) * rateOfNextThreeHundredUnits) + (Float(units - 500) * rateAboveFiveHundredUnits)
     }
     let finalAmount = amount + serviceCharge
     print("Bill to be paid is Rs.",finalAmount)
 }
-if billType == "commercial" {
+func commercial() {
+    var amount : Float = 0
+    var perDayUnitForTwoMonths: [Int] = []
     let serviceCharge: Float = 290
+    let  rateOfFirstHundredUnits: Float = 5.00
+    let rateAboveHundredUnits: Float = 8.05
     for each in 0..<60 {
         let perDayUnit = Int.random(in:10...100)
         perDayUnitForTwoMonths.append(perDayUnit)
@@ -48,11 +58,18 @@ if billType == "commercial" {
     let units = perDayUnitForTwoMonths.reduce(0,+)
     print("Units:",units)
     if units <= 100 {
-        amount = Float(units) * 5.00
+        amount = Float(units) *  rateOfFirstHundredUnits
     }
     else {
-        amount = (Float(100) * 5.00) + (Float(units - 100) * 8.05 )
+        amount = Float(100 *  rateOfFirstHundredUnits) + (Float(units - 100) * rateAboveHundredUnits )
     }
     let finalAmount = amount + serviceCharge
     print("Bill to be paid is Rs.",Float(finalAmount))
+}
+let billType = "domestic"
+if billType == type.domestic.rawValue {
+    domestic()
+}
+else {
+    commercial()
 }
