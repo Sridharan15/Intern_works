f1d0816476c6b8a8fa9b416c6d5592d62b2661dd
Sridharan T <51164940+Sridharan15@users.noreply.github.com>
2020-04-21 17:54:31 +0530

    Electricity Bill Calculatot

--- electricityBillCalculatorWithEnums.swift
+++ electricityBillCalculatorWithEnums.swift
@@ -1,3 +1,18 @@
+/*Problem Statement : Need to calculate Electricity bill for every two months based on below tariffs.  
+
+For domestic
+For first 100 units Rs 0/unit
+For next 100 units Rs 3.50/unit
+For next 300 units Rs 4.60/unit
+For unit above 500  Rs 6.60/unit
+Fixed charges for two months Rs.50/service
+
+For Commercial
+For first 100 units Rs 5.00/unit
+Consumption above 100 units bi monthly 8.05/unit
+Fixed charges for two months Rs.290/service
+Get random numbers as daily consumable units. For domestic random number generation should be in between 1 - 10. For commercial 10 - 100*/
+
 enum BillType {
     case domestic, commercial
 }
